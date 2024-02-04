a19fa18fdadb4b08f1a3a2add242a2e80a3055c9
Pashtet <ptashkaaaaaa@gmail.com>
2024-02-04 17:53:05 +0100

    fix: remove karma and add jest

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+
+const config: Config = {
+    clearMocks: true,
+    collectCoverage: true,
+    preset: 'jest-preset-angular',
+    setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    testEnvironment: 'jsdom',
+};
+
+export default config;
