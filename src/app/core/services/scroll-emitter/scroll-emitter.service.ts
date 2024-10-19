29f48d4986514bf601c59d315b7373abe778f237
Pashtet <ptashkaaaaaa@gmail.com>
2024-10-19 17:57:34 +0200

    feat: add products-nav preview

--- src/app/core/services/scroll-emitter/scroll-emitter.service.ts
+++ src/app/core/services/scroll-emitter/scroll-emitter.service.ts
@@ -8,29 +8,31 @@ import { map } from 'rxjs/operators';
 })
 export class ScrollEmitterService {
     private _isOnTop$: BehaviorSubject<boolean> = new BehaviorSubject(true);
+
     public isOnTop$() {
         return this._isOnTop$.asObservable();
     }
 
     constructor(
-        @Inject(DOCUMENT) private document: any,
+        @Inject(DOCUMENT) private document: Document,
         @Inject(PLATFORM_ID) private platformId: any
     ) {
         if (isPlatformBrowser(this.platformId)) {
-            const windowPosition = window.scrollY || this.document.documentElement.scrollTop;
+            const getWindowScrollPosition = () => window.scrollY || this.document.documentElement.scrollTop;
+            const windowPosition = getWindowScrollPosition();
             if (windowPosition !== 0) this._isOnTop$.next(false);
 
-            fromEvent(window, 'scroll').pipe(
-                map(
-                    () => window.scrollY || this.document.documentElement.scrollTop
-                ),
-                debounceTime(20),
-            ).subscribe(((scrollPosition) => {
-                const previousIsOnTotValue = this._isOnTop$.value;
+            fromEvent(window, 'scroll')
+                .pipe(
+                    map(() => getWindowScrollPosition()),
+                    debounceTime(20)
+                )
+                .subscribe((scrollPosition) => {
+                    const previousIsOnTopValue = this._isOnTop$.value;
 
-                if (scrollPosition === 0 && !previousIsOnTotValue) this._isOnTop$.next(true);
-                if (scrollPosition !== 0 && previousIsOnTotValue) this._isOnTop$.next(false);
-            }));
+                    if (scrollPosition === 0 && !previousIsOnTopValue) this._isOnTop$.next(true);
+                    if (scrollPosition !== 0 && previousIsOnTopValue) this._isOnTop$.next(false);
+                });
         }
     }
 }
