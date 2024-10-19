ee3c69bdd50f279ec4fbde80e024bd13e5284f10
Pashtet <ptashkaaaaaa@gmail.com>
2024-10-19 19:50:21 +0200

    fix: change header-menu to render component on hover

--- src/app/core/components/header-menu/header-menu.component.ts
+++ src/app/core/components/header-menu/header-menu.component.ts
@@ -1,3 +1,4 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
@@ -6,10 +7,11 @@ import { SubMenuLinkContent } from '@core/types/sub-menu-link-content.interface'
 @Component({
     selector: 'app-header-menu',
     standalone: true,
-    imports: [MatIcon, RouterLink],
+    imports: [MatIcon, RouterLink, NgClass],
     templateUrl: './header-menu.component.html',
     styleUrl: './header-menu.component.scss',
 })
 export class HeaderMenuComponent {
     @Input({ required: true }) menuItems!: SubMenuLinkContent[];
+    protected readonly onmouseenter = onmouseenter;
 }
