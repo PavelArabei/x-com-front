1b1c73a0fc34a7ade5dfbe64544c119f8516aacc
Pashtet <ptashkaaaaaa@gmail.com>
2024-11-10 18:29:34 +0100

    fix: fix header-menu.component

--- src/app/core/components/header-menu/header-menu.component.ts
+++ src/app/core/components/header-menu/header-menu.component.ts
@@ -1,17 +1,30 @@
 import { NgClass } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { AppRoutes } from '@app/app.routes';
 import { SubMenuLinkContent } from '@core/types/sub-menu-link-content.interface';
+import { ButtonLinkComponent } from '@shared/components/button-link/button-link.component';
 
 @Component({
     selector: 'app-header-menu',
     standalone: true,
-    imports: [MatIcon, RouterLink, NgClass],
+    imports: [MatIcon, RouterLink, NgClass, ButtonLinkComponent],
     templateUrl: './header-menu.component.html',
     styleUrl: './header-menu.component.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderMenuComponent {
-    @Input({ required: true }) menuItems!: SubMenuLinkContent[];
-    protected readonly onmouseenter = onmouseenter;
+    protected subCategoryRouterPath = AppRoutes.SUBCATEGORY;
+
+    @Input() menuItems: SubMenuLinkContent[] | null = null;
+    @Input({ required: true }) title!: string;
+    @Input() nestingLevel: number = 0;
+    @Input() additionalRoutePath: string | null = null;
+
+    getRouterLink(route: string): string {
+        return this.additionalRoutePath
+            ? `/${this.additionalRoutePath}/${route}`
+            : `/${route}`;
+    }
 }
