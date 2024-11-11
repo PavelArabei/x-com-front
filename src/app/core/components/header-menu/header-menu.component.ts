68b762643d825c2e013cff3737c9fb5e0d046f3a
Pashtet <ptashkaaaaaa@gmail.com>
2024-11-11 16:29:40 +0100

    feat: simplify header menu

--- src/app/core/components/header-menu/header-menu.component.ts
+++ src/app/core/components/header-menu/header-menu.component.ts
@@ -2,7 +2,6 @@ import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
-import { AppRoutes } from '@app/app.routes';
 import { SubMenuLinkContent } from '@core/types/sub-menu-link-content.interface';
 import { ButtonLinkComponent } from '@shared/components/button-link/button-link.component';
 
@@ -15,11 +14,8 @@ import { ButtonLinkComponent } from '@shared/components/button-link/button-link.
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderMenuComponent {
-  protected subCategoryRouterPath = AppRoutes.SUBCATEGORY;
-
   @Input() menuItems: SubMenuLinkContent[] | null = null;
   @Input({ required: true }) title!: string;
-  @Input() nestingLevel: number = 0;
   @Input() additionalRoutePath: string | null = null;
 
   getRouterLink(route: string): string {
