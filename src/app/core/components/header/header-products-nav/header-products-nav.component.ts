2f649fbf3ef27fdc42fefd655a0750cb4aef27ea
Pashtet <ptashkaaaaaa@gmail.com>
2024-11-11 16:28:00 +0100

    refactor: simplify menu

--- src/app/core/components/header/header-products-nav/header-products-nav.component.ts
+++ src/app/core/components/header/header-products-nav/header-products-nav.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AppRoutes } from '@app/app.routes';
-import { computerComponents } from '@assets/mock/sub-categories/computer-components-category';
+import { computerComponents } from '@assets/mock/computer-components-category';
 import { HeaderLinkComponent } from '@core/components/header-link/header-link.component';
 import { HeaderMenuComponent } from '@core/components/header-menu/header-menu.component';
 
@@ -12,6 +12,12 @@ import { HeaderMenuComponent } from '@core/components/header-menu/header-menu.co
   styleUrl: './header-products-nav.component.scss',
 })
 export class HeaderProductsNavComponent {
-  protected readonly routerPath = AppRoutes.CATEGORY;
+  protected readonly subcategoryRoute = AppRoutes.SUBCATEGORY;
+  protected readonly categoryRoute = AppRoutes.CATEGORY;
+
   protected readonly products = Object.values(computerComponents);
+
+  protected getCategoryRoute(category: string): string {
+    return `/${this.categoryRoute}/${category}`;
+  }
 }
