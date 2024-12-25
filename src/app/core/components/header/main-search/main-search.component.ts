35333b5f58e77251af2e8e02b83e9543d09f7b59
Pashtet <ptashkaaaaaa@gmail.com>
2024-12-25 18:17:20 +0100

    feat: change main search component

--- src/app/core/components/header/main-search/main-search.component.ts
+++ src/app/core/components/header/main-search/main-search.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatFabButton, MatIconButton } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-main-search',
   standalone: true,
-  imports: [MatIcon, FormsModule],
+  imports: [
+    MatIcon,
+    FormsModule,
+    MatFormFieldModule,
+    MatInput,
+    MatIconButton,
+    MatFabButton,
+  ],
   templateUrl: './main-search.component.html',
   styleUrl: './main-search.component.scss',
 })
-export class MainSearchComponent {}
+export class MainSearchComponent {
+  searchText = '';
+}
